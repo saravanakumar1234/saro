c38363569d842476161f0e37d2fe04ce26a846f2
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-01-17 16:13:45 +0530

    a.c

---
+++ a.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,deg,pow;
+clrscr();
+printf("enter the power ");
+scanf("%d",&pow);
+for(i=0;i<=n;i++)
+{
+printf("enter the deg");
+scanf("%d",&deg);
+}
+if
+{
+sum=deg^pow;
+}
+getch();
+}
+
