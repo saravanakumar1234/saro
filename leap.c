b0501db1f83f2047d0d8cd9f7f9e44ef633b80fe
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-01-17 16:48:49 +0530

    leap.c

---
+++ leap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n;
+clrscr();
+  printf("enter the value");
+  scanf("%d",&n);
+if(n%4==0)
+{
+printf("it is a leap year");
+}
+  else
+{
+printf("it is not a leap year");
+}
+getch();
+}
