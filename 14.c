79bb9ead05831ee6869e3beceff7f8dfafc2bdb7
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-01-24 16:01:21 +0530

    14.c

---
+++ 14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int  main()
+{
+int n,sum=0,i=1;
+printf("enter the value of n");
+scanf("%d",&n);
+while(i<=n)
+{
+if(i%2==1)
+}
+printf("the odd numbers between two intervals %d is:%d",n ,sum);
+retuen 0;
+}
