436a3621ab6c5fd60e5c89a2f6a074a87281fe5d
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-02 16:28:35 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ void main()
+ {
+   int i,count=0;
+   char a[100];
+   gets(a);
+   for(i=0 ; a[i]!='\0' ; i++)
+     {
+     	if(a[i]==' ')
+     	{
+             count++;
+     	}
+     }
+   printf("%d\n",count+1);
+ }
