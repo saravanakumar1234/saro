05747803df3c8df62237da9410f9012a50a7e02e
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-02 16:08:51 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,m,a,b;
+printf("enter the first time");
+scanf("%d%d",&i,&j);
+printf("enter the second time");
+scanf("%d%d",&k,&m);
+a=i-k;
+b=j-m;
+printf("the time is %d%d",a,b);
+}
