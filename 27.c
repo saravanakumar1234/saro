bff111fbcbc03824617f5e06a54851e94e537293
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-02 15:50:05 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the value of string  a:");
+scanf("%d",&a);
+if(a>0)
+printf("yes the given string a is numeric");
+else
+printf("the string is not numeric");
+return 0;
+}
