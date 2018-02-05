737d7cf3d38f97ad3151d6d9565e76f9388b6b83
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-05 15:54:40 +0530

    Rename 36.c to 366.c

---
+++ 366.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char a[100]="abc123@$";
+int i,count=0;
+clrscr();
+for(i=0;a[i]!='\0';i++)
+{
+if((a[i]>='a' && a[i]<='z') || (a[i]>='A' && a[i]<='Z'))
+{
+else if((a[i]>='0' && a[i]<='9'))
+{
+printf("no special character");
+}
+else
+{
+count++;}}
+printf("special character %d",count);
+}
+getch();
+}
