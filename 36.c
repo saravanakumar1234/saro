59a2c354b576e732305ac4d3e533f2f8980ab90a
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-05 15:50:54 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100]="abc123@$";
+int i,count=0;
+for(i=0;a[i]!='\0';i++)
+{
+if((a[i]>='a' && a[i]<='z') || (a[i]>='A' && a[i]<='Z'))
+{
+else if((a[i]>='0' && a[i]<='9'))
+{
+printf("no special character");
+}
+else
+{
+count++;}}
+printf("special character %d",count);
+}
+}
