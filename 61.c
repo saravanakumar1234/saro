7060de4253d8aef9abd348bf962cd26e55ed6492
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-21 15:48:57 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char s[20];
+int k,i;
+printf("enter the string");
+scanf("%s",&s[i]);
+printf("enter the number");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+printf("%c",s[i]);
+}
