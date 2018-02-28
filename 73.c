90df17e6787a1ad4ddd9a62d80eff43982e51702
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-02-28 16:28:57 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int n,s,r;
+    printf("enter the values");
+    scanf("%d,%d,%d",&n,&s,&r);
+    if(n>=s&&n<=r)
+    printf("yes");
+    else
+    printf("no");
+}
