7c98439c141239f2b6e6f499969a0126e99b134d
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-03-07 16:01:42 +0530

    Rename 72.c to 75.c

---
+++ 75.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char a[10],i;
+    printf("enter the a\n");
+    scanf("%s",&a[i]);
+    for(i=0;i<5;i++)
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+        printf("yes");
+        else
+        printf("no");
+}
