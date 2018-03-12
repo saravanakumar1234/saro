abb3acd3d0811c1af2b2aedf398d30d0f984c837
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-03-12 15:58:21 +0530

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char r[50];
+	int i,len;
+	printf("enter the string\n");
+	scanf("%s",r);
+	len=strlen(r);
+	for(i=0;i<len;i=i+2)
+	printf("%c",r[i]);
+	for(i=1;i<len;i=i+2)
+		printf("%c",r[i]);
+}
+	
