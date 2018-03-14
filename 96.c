d61b4d343917d803b5d093785afd6eb8b09667dc
saravanakumar1234 <35525129+saravanakumar1234@users.noreply.github.com>
2018-03-14 16:16:23 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,i,c=0;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+c++;
+}
+if(c==2)
+printf("no");
+if(c!=2)
+printf("yes");
+}
